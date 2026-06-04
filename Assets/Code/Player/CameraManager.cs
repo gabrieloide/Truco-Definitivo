28597030466740388b0883efac7c1938b6fe088e
Gabriel <gabrielfuentes7312@gmail.com>
2026-06-04 01:47:49 -0400

    Fix: Add Code.GameLogic namespace to CameraManager

--- Assets/Code/Player/CameraManager.cs
+++ Assets/Code/Player/CameraManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine; // Adjust namespace if using an older Cinemachine version
+using Code.GameLogic;
 
 namespace Code.Player
 {
